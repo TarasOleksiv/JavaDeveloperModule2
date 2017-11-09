f2affef0fd22b564e06c68908454322483ca5312
Taras Oleksiv <toleksiv69@gmail.com>
2017-11-09 23:43:01 +0200

    Menu changes.

--- src/main/java/ua/goit/java8/javadeveloper/view/DevelopersMenu.java
+++ src/main/java/ua/goit/java8/javadeveloper/view/DevelopersMenu.java
@@ -1,5 +1,9 @@
 package ua.goit.java8.javadeveloper.view;
 
+import ua.goit.java8.javadeveloper.dao.jdbc.JdbcDeveloperDAOImpl;
+import ua.goit.java8.javadeveloper.model.Developer;
+
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -15,17 +19,37 @@ public class DevelopersMenu {
     private void show(){
         System.out.println();
         System.out.println("Меню Developers");
-        System.out.println("Які дії виконуєм? (1 - Вивести всіх девелоперів, інший символ - Повернутись у Головне меню)");
+        System.out.println("Які дії виконуєм? (" +
+                "1 - Вивести всіх девелоперів, " +
+                "2 - Вивести девелопера по id, " +
+                "3 - Створити девелопера, " +
+                "4 - Оновити девелопера, " +
+                "5 - Вилучити девелопера, " +
+                "інший символ - Повернутись у Головне меню)");
         System.out.print("Введіть символ: ");
         String n = sc.nextLine().trim();
         switch (n) {
             case "1":
-                //chooseCarTask();
+                getAll();
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
                 break;
             default:
                 System.out.println("Повернення у Головне меню");
                 return;
         }
         show();
+    }
+
+    private void getAll(){
+        JdbcDeveloperDAOImpl jdbcDeveloperDAO = new JdbcDeveloperDAOImpl();
+        List<Developer> developers = jdbcDeveloperDAO.getAll();
+
     }
 }
--- src/main/java/ua/goit/java8/javadeveloper/view/MainMenu.java
+++ src/main/java/ua/goit/java8/javadeveloper/view/MainMenu.java
@@ -15,12 +15,30 @@ public class MainMenu {
     private void show(){
         System.out.println();
         System.out.println("Головне меню.");
-        System.out.println("З яким об'єктом працюєм? (1 - Developers, інший символ - Вихід з програми)");
+        System.out.println("З яким об'єктом працюєм? (" +
+                "1 - Companies, " +
+                "2 - Customers, " +
+                "3 - Skills, " +
+                "4 - Developers, " +
+                "5 - Projects, " +
+                "інший символ - Вихід з програми)");
         System.out.print("Введіть символ: ");
         String n = sc.nextLine().trim();
         switch (n) {
-            case "1":
+            case "1":   //Companies
+                //new CompaniesMenu();
+                break;
+            case "2":   //Customers
+                //new CustomersMenu();
+                break;
+            case "3":   //Skills
+                //new SkillsMenu();
+                break;
+            case "4":   //Developers
                 new DevelopersMenu();
+                break;
+            case "5":   //Projects
+                //new ProjectsMenu();
                 break;
             default:
                 System.out.println("Ви закінчили виконання програми.");
